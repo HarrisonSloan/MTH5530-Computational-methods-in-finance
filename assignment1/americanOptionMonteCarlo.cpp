8010cdab64f094d224a362d351678f4fd3bb1404
Harrison Sloan <haza7200@gmail.com>
2025-04-25 03:11:38 +1000

    Developed working monte carlo sim of American put option, need to do some verification still

--- assignment1/americanOptionMonteCarlo.cpp
+++ assignment1/americanOptionMonteCarlo.cpp
@@ -11,8 +11,10 @@
 #include <boost/random/variate_generator.hpp>
 #include <thread>
 #include <mutex>
+#include <Eigen/Dense>
 
 using namespace std;
+using Eigen::MatrixXd;
 
 double identity(double x) {
     return x;
@@ -131,10 +133,7 @@ class HestonSimulator {
                 //
                 //times[i] = times[i-1]+delta;
             }
-            // Retrieve final prices add to running total and average it
-            mtx.lock();
-            optionPricePayOffSum+=max((double)0, pow(EulerConstant, logPrices[startIndex+(int)(time*stepsPerYear)])-strike);
-            mtx.unlock();       
+            // Retrieve final prices add to running total and average it    
         }
 
         // Note we may start to have errors where the time steps per year into how many "years" we have is no longer an integer
@@ -171,14 +170,116 @@ class HestonSimulator {
                 thread.join();
             }
 
-            if (reuseRNG) {
-                tempFinalPrice = pow(EulerConstant,-interestRate*time) * (1.0/paths) * optionPricePayOffSum;
-                cout << " Different Computed price is " << tempFinalPrice << "\n";
-                return;
+            // Now need to backwards induct on the set of in path 
+            // So keep of track of ones that are firstly being 
+            // Continued, if continued and in money then include in regression
+
+
+            // Logic!!!!
+            // Check if its in path (K-discounted at S_t)^+, if this is more than 0, we will include in the regresison
+
+            // what I need, latest g_k, the associated times this occured tao that sits wit hteh tao_k 
+            // Each time find hte in money paths, retrieve their g_k and discount by the tao_k amount (the time difference between tao_j and where we are now)
+            // complete a linear regression for hte subset
+            // tricky bit is how do I correctly map back the values 
+            // I then need to see what the predicted values are, if its better update g_k and tao_k
+
+            // Payoff values
+            auto terminalPayoffs = std::make_unique<double[]>(paths);
+            auto terminalTimes = std::make_unique<double[]>(paths);
+            for (int i = 0; i<paths; i++) {
+                // Go to the end price
+                terminalPayoffs[i] = putPayoff(this->logPrices[(i+1)*this->time*this->stepsPerYear + i]);
+                // Time is measured here in just amount of intervals but corrected down the line
+                terminalTimes[i]=this->time*this->stepsPerYear;
             }
-            finalPrice = pow(EulerConstant,-interestRate*time) * (1.0/paths) * optionPricePayOffSum;
-            cout << " Computed price is " << finalPrice << "\n";
-            return;
+            
+
+            // intialise the above
+
+            // In money subset 
+            // regression pointers, this is to say what path is being used in the regression
+            // For example if we have 10 paths but only 4 used in the regressions say (0,5,6,8)
+            
+            auto regressionPointers = std::make_unique<int[]>(paths);
+
+            const int features = 6;
+            // regression will be a 
+            // # in money paths * 6 matrix 
+            // int i = (int)(time*stepsPerYear) vs int i = (int)(time*stepsPerYear)-1
+            double delta = 1 / (double)this->stepsPerYear;
+            for (int i = (int)(time*stepsPerYear)-1; i>=1; i--) {
+                // For each path determine if the new price is in money
+                int numPathInMoney = 0;
+                for (int pathNum = 0; pathNum<paths;pathNum++) {
+                    int pathIndex = pathNum*time*stepsPerYear + pathNum + i;
+                    // if an in money path
+                    if (putPayoff(this->logPrices[pathIndex])>0) {
+                        regressionPointers[pathNum] = numPathInMoney;
+                        numPathInMoney++;
+                    } else {
+                        regressionPointers[pathNum] = -1;
+                    }
+
+                    // Create the X matrix and Y matrix for the regression
+                }
+                cout << "Total in money paths " << numPathInMoney << "\n";
+                Eigen::MatrixXd X(numPathInMoney, features);
+                Eigen::VectorXd Y(numPathInMoney);
+                for (int pathNum = 0; pathNum<paths;pathNum++) {
+                    if (regressionPointers[pathNum] != -1) {
+                            int pathIndex = pathNum*time*stepsPerYear + pathNum + i;
+                            double price = pow(this->EulerConstant,this->logPrices[pathIndex]);
+                            double variance = variances[pathIndex];
+                            X(regressionPointers[pathNum],0) = 1;
+                            X(regressionPointers[pathNum],1) = price; // S
+                            X(regressionPointers[pathNum],2) = pow(price,2); // S^2
+                            X(regressionPointers[pathNum],3) = variance;
+                            X(regressionPointers[pathNum],4) = pow(variance,2);
+                            X(regressionPointers[pathNum],5) = price*variance;
+                            // Discounted g_x
+                            Y(regressionPointers[pathNum]) = pow(this->EulerConstant,-this->interestRate*(terminalTimes[pathNum]-i)*delta)*terminalPayoffs[pathNum];
+                    }
+                }
+                // Run the regression 
+                Eigen::VectorXd beta = X.colPivHouseholderQr().solve(Y);
+                Eigen::VectorXd continuationValues = X * beta;
+                cout << " Betas 1 " << beta(0) << " Betas 2 " << beta(1) << " Betas 3 " << beta(2) << " Betas 4 " << beta(3) << " Betas 5 " << beta(4) << " Betas 6 " << beta(5) << "\n";
+                // Compute R²
+                double yMean = Y.mean();
+                double ssTotal = (Y.array() - yMean).square().sum();
+                double ssResidual = (Y - continuationValues).squaredNorm();  // same as ∑ (Y_i - Ŷ_i)^2
+
+                double rSquared = 1.0 - (ssResidual / ssTotal);
+
+                // Optional: Clamp to [0, 1] in case of tiny numerical noise
+                rSquared = std::max(0.0, std::min(1.0, rSquared));
+
+                std::cout << "R² at timestep " << i << " = " << rSquared << "\n";
+                // Determine the set where the payoff is larger than the continuation value
+                for (int pathNum = 0; pathNum<paths;pathNum++) {
+                    if (regressionPointers[pathNum] != -1) {
+                        int pathIndex = pathNum*time*stepsPerYear + pathNum + i;
+                        double newPayOff = putPayoff(this->logPrices[pathIndex]);
+                        if (continuationValues(regressionPointers[pathNum]) < newPayOff) {
+                            terminalPayoffs[pathNum] = newPayOff;
+                            terminalTimes[pathNum] = i;
+                        }
+                    }
+                    // reset the regression pointer
+                    regressionPointers[pathNum] = -1;
+                }
+            }
+
+            double continuationValuesSum = 0;
+            for (int pathNum = 0; pathNum<paths;pathNum++) {
+                continuationValuesSum += pow(this->EulerConstant,-this->interestRate*(terminalTimes[pathNum])*delta)*terminalPayoffs[pathNum];
+            }
+            double continuationAverage = continuationValuesSum / this->paths;
+            finalPrice = max(max(0.0,this->strike-initialStockPrice),continuationAverage);
+            cout << "Final price is " << finalPrice << "\n";
+    
+            
         }
 
         void intialRun() {
@@ -190,22 +291,6 @@ class HestonSimulator {
             // TODO 
         }
 
-        void deltaAndGamma() {
-            // Delta = F(t, intialPrice + small amount) - F(t, intialPrice - small amount)  / 2h
-            // Gama = F(t, intialPrice + small amount) - F(t, intialPrice - small amount) + 2 * oriingla Price / h^2
-            if (!simulationRan) {
-                throw("Run intial simulation");
-            }
-            double normal = finalPrice;
-            double change = 0.01;
-            run(change,true);
-            double high = tempFinalPrice;
-            run(-change,true);
-            double low = tempFinalPrice;
-            double delta = (high - low) / (2.0 * change);
-            double gamma = (high + low - 2.0 * normal) / (change * change);
-            cout << " delta is " << delta << " Gamma is " << gamma << "\n";
-        }
     private:
         // random number generations -> alternatively we apply box muller transform to achieve same result
         mutex mtx; // lock for random number generation
@@ -214,6 +299,13 @@ class HestonSimulator {
         boost::normal_distribution<> nd;
         boost::variate_generator<boost::mt19937&, boost::normal_distribution<> > gen1;
         boost::variate_generator<boost::mt19937&, boost::normal_distribution<> > gen2;
+
+        double putPayoff(double logPrice) {
+            return max(0.0,this->strike-pow(this->EulerConstant,logPrice));
+        }
+        double basisFunc(double a, double b, double c, double d, double e, double f, double price, double variance) {
+            return a+b*price+c*pow(price,2)+d*variance+d*pow(variance,2)+f*price*variance;
+        }
     };
 
 // TODO 
@@ -229,23 +321,11 @@ int main() {
     double rateOfReversion = 2; 
     double volOfVol = 1;
     double time = 5;
-    int stepsPerYear = 40;
-    int numPaths = 40000;
+    int stepsPerYear = 50;
+    int numPaths = 10000;
     HestonSimulator absorptionSim = HestonSimulator(time,interestRate,strike,initialStockPrice,initialVariance,longTermVariance,corr,rateOfReversion,volOfVol,stepsPerYear,numPaths,identity, positivePart, positivePart);\
     absorptionSim.intialRun();
-    absorptionSim.deltaAndGamma();
-    // std::vector<std::vector<double>> allPaths(numPaths);
-    // std::vector<double> timeVec(stepsPerYear*time);
-    // for (int i = 0; i < (int)(time*stepsPerYear); i++) {
-    //     timeVec[i] = i * (1 / (double)stepsPerYear);
-    // }
-
-    // for (int i = 0; i < numPaths; i++) {
-    //     absorptionSim.run(positivePart, positivePart, positivePart);
-    //     allPaths[i] = std::vector<double>(absorptionSim.logPrices.get(), absorptionSim.logPrices.get() + (int)(stepsPerYear*time));
-    // }
-
-    // matplot::figure()->size(1920,1080);
+    matplot::figure()->size(1920,1080);
     // matplot::hold(true);  // keep all lines on the same figure
     // for (const auto& path : allPaths) {
     //     matplot::plot(timeVec, path);
@@ -253,7 +333,7 @@ int main() {
     // matplot::title("Monte Carlo sim Pricing");
     // matplot::xlabel("M-time steps");
     // matplot::ylabel("log option prices");
-    // matplot::show();
+    matplot::show();
     // matplot::save("plot.png");
     return 0;
 }
